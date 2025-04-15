8e1fd34011b46085d2e9dab081ac2d46e8f043a2
Phiranath PO-NGERN <phukaow18@gmail.com>
2025-04-15 21:06:18 +0700

    feat: initial webpage for api and DA page

--- src/pages/api.py
+++ src/pages/api.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.title("Welcome to Thailand Accident Analysis Web Page:car:")
+st.title("Welcome to API Page:car:")
 st.markdown(
     """
-    This dashboard helps explore car accident statistics across Thailand.  
+    This page helps explore the API with the example of the results after calling the API.  
     Use the sidebar to select a specific analysis page.
     """
 )
---
+++ src/pages/data_analytic.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Welcome to Data Analytic Page:car:")
+st.markdown(
+    """
+    This page shows the process of data analytic from preprocessing until the model training.
+    """
+)
