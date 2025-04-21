bb0ad29dfbfc438f5916d2d99cbc51600a405104
Nunthapop Nganiam <nuntha_pop@hotmail.com>
2025-04-21 23:44:11 +0700

    fix: add code block, and it output from the data analysis notebook

--- src/pages/data_analytic.py
+++ src/pages/data_analytic.py
@@ -11,3 +11,10 @@ st.markdown(
     This page shows the process of data analytic from preprocessing until the model training.
     """
 )
+
+with open("src/notebook/Data-Analysis-Notebook.md", "r", encoding="utf-8") as f:
+    markdown_content = f.read()
+
+st.markdown(markdown_content, unsafe_allow_html=True)
+
+
